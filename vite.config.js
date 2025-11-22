5ef810a57f355fa3aa4318202ffa15f5e43b783e
ukiuni <ukiuni@gmail.com>
2025-11-22 13:30:45 +0900

    Add custom domain configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/freeicons_claude_sonnet_4/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
